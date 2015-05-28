1bf0cec6beb3cf6d5a83957c4243bf514e4df5b0
David Higginbotham <davehigginbotham@gmail.com>
2015-05-27 22:58:37 -0600

    kss isnt kicking my ass anymore :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
     gless = require('gulp-less'),
-    gkss = require('gulp-kss'),
     gconcat = require('gulp-concat'),
     gshell = require('gulp-shell'),
     gclean = require('gulp-clean'),
@@ -11,12 +10,7 @@ var gulp = require('gulp'),
 
 gulp.task('less', function() {
   gulp.src('less/style.less')
-      .pipe(gless({
-        paths: [
-          path.join(__dirname, 'less', 'components'),
-          path.join(__dirname, 'less', 'templates')
-        ]
-      }))
+      .pipe(gless())
       .pipe(gminifyCss())
       .pipe(gconcat('style.min.css'))
       .pipe(gulp.dest('dist'))
@@ -24,33 +18,32 @@ gulp.task('less', function() {
       .on('error', gutil.log);
 });
 
-// this is the kss gulp task we run to generate
-// the styleguide for BASELESS
-gulp.task('kss', function() {
-  // clean our styleguide folder out before
-  // we create the styleguide.
-  gulp.src(['styleguide/*', 'styleguide/**/*'], {read: false})
-      .pipe(gclean({force: true}));
+gulp.task('styleguide:clean', function() {
+  gulp.src(['styleguide/**/*'], {read: false})
+    .pipe(gclean({force: true}));
+});
 
-  // runs through all our less files to make
-  // our styleguide
-  gulp.src(['less/*.less', 'less/**/*.less'])
-      .pipe(gkss({
-        overview: __dirname + '/less/styleguide.md'
-      }))
-      .on('error', gutil.log)
-      .pipe(gulp.dest('styleguide/'));
-
-  // build our less so we get context to 
-  // the components to style
+gulp.task('styleguide:less', function() {
   gulp.src('less/style.less')
       .pipe(gless())
       .pipe(gconcat('public/style.css'))
       .pipe(gulp.dest('styleguide/'));
 });
 
+gulp.task('styleguide:kss', gshell.task([
+    'kss-node <%= source %> <%= destination %> --template <%= template %> --css <%= css %>'
+  ], {
+    templateData: {
+      source:       path.join(__dirname, 'less'),
+      destination:  path.join(__dirname, 'styleguide'),
+      template:     path.join(__dirname, 'less', 'templates', 'styleguide'),
+      css:          path.join(__dirname, 'dist', 'style.min.css')
+    }
+  }
+));
+
 gulp.task('watch', function() {
-  gulp.watch('less/**/*.less', ['less']);
+  gulp.watch('less/**/*.less', ['less', 'styleguide:kss']);
 });
 
-gulp.task('go', ['less', 'watch']);
+gulp.task('go', ['less', 'styleguide:kss', 'watch']);
