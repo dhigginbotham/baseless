d276ea14a2dc9b3f027f499d685edd9243a98e52
David Higginbotham <davehigginbotham@gmail.com>
2015-06-02 18:12:10 -0600

    added grunt tasks, they should all be in parody sans screenshots, which may be getting dropped anyways

--- gruntfile.js
+++ gruntfile.js
@@ -49,6 +49,16 @@ module.exports = function(grunt) {
       less: {
         command: grunt.config.process(tasks.kss.less.exec)
       }
+    },
+    parker: {
+      options: tasks.parker.opts,
+      src: [tasks.parker.src]
+    },
+    'gh-pages': {
+      options: {
+        base: 'styleguide/'
+      },
+      src: ['**']
     }
   });
 
@@ -58,11 +68,18 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-parker');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   // register grunt task
-  grunt.registerTask('default', ['less:dist', 'less:styleguide', 'cssmin:dist']);
   grunt.registerTask(tasks.clean.name, ['clean']);
+  grunt.registerTask(tasks.dist.name, ['less:dist', 'cssmin:dist']);
+  grunt.registerTask(tasks.less.name, ['less:styleguide']);
   grunt.registerTask(tasks.readme.name, ['copy:readme']);
   grunt.registerTask(tasks.kss.name, ['shell:kss']);
   grunt.registerTask(tasks.kss.less.name, ['shell:less']);
+  grunt.registerTask(tasks.parker.name, ['parker']);
+  grunt.registerTask(tasks.ghpages.name, ['gh-pages']);
+  grunt.registerTask('default', [tasks.clean.name, tasks.dist.name, tasks.less.name, tasks.kss.name]);
+  grunt.registerTask('styleguide', [tasks.dist.name, tasks.less.name, tasks.readme.name, tasks.kss.name]);
 };
